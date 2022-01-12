411b49ad395602f54c7aaf2efa3e0e42bfa7d030
Cyril <tourniercy@gmail.com>
2022-01-12 14:21:51 +0100

    Updates

---
+++ precache-manifest.c5a36afe3861d4ad3b6997b973a30bcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a7eae96a34b347697192322538e6c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3a4dfe879e7f80f48b78",
+    "url": "./static/css/2.9c389199.chunk.css"
+  },
+  {
+    "revision": "7df7267b1d1b7803910a",
+    "url": "./static/css/main.8653b40b.chunk.css"
+  },
+  {
+    "revision": "3a4dfe879e7f80f48b78",
+    "url": "./static/js/2.38a9b909.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "./static/js/2.38a9b909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7df7267b1d1b7803910a",
+    "url": "./static/js/main.41bfc19c.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
